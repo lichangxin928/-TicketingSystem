846214c010a795a2356c71f3c182d00dec4ed6a2
lcx <2431424266@qq.com>
2021-06-07 19:16:31 +0800

    6.7日 总结

--- source/db.js
+++ source/db.js
@@ -14,8 +14,8 @@ module.exports = (sql, data = []) => {
     return new Promise((resolve, reject) => {
     //在数据池中进行操作
         pool.query(sql, data, function(error, results, fields) {
-            if (error) {
-                reject(error.message)
+            if (error) { 
+                reject(error.message) 
             } else {
                 resolve(results)
             }
